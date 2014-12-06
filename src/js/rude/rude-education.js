456be1a623b99d9e84435f42f7034cda5033506c
look <thisnicknamewasfree@gmail.com>
2014-12-06 12:10:10 +0000

    merged

--- src/js/rude/rude-education.js
+++ src/js/rude/rude-education.js
@@ -20,16 +20,223 @@ var education =
 
 	filler:
 	{
-		popup: function(disciplines)
+		database: null,
+		name: null,
+
+		popup: function(disciplines, name)
 		{
-			$('.content').html('');
-			for (var i=0; i<disciplines.length; i++){
-				$('.content').append(disciplines[i]);
-			}
-			$('#filler-modal').modal('show');
+			education.filler.database = disciplines;
+			education.filler.name = name;
 
+			debug(name);
 
+			education.filler.update();
+
+			$('#filler-modal').modal('show').modal('cache sizes');
+
+			setTimeout(function() {
+				$('#filler-modal').modal('refresh');
+			}, 750);
 		},
+
+		update: function()
+		{
+			var html = '';
+
+			html += '<table class="ui basic table plan">';
+			html += '<tbody>';
+			html += '	<tr>';
+			html += '		<td rowspan="4">';
+			html += '		№ <nobr>п/п</nobr>';
+			html += '		</td>';
+			html += '		<td rowspan="4">Название цикла, интегрированного модуля, учебной дисциплины, курсовой работы (проекта)</td>';
+			html += '		<td rowspan="4">Кафедра</td>';
+				
+			html += '		<td rowspan="1" colspan="9"><div>Количество академических часов</div></td>';
+			html += '		<td rowspan="1" colspan="30"><div>Распределение по курсам и семестрам</div></td>';
+			html += '	</tr>';
+				
+			html += '	<tr>';
+			html += '		<td rowspan="3" class="rotate-box"><div class="rotate-270">Экзаменов</div></td>';
+			html += '		<td rowspan="3" class="rotate-box"><div class="rotate-270">Зачётов</div></td>';
+			html += '		<td rowspan="3" class="rotate-box"><div class="rotate-270">Курсовых проектов</div></td>';
+			html += '		<td rowspan="3" class="rotate-box"><div class="rotate-270">Расчётных работ</div></td>';
+			html += '		<td rowspan="3" class="rotate-box"><div class="rotate-270">Типовых расчётов / контрольных</div></td>';
+			html += '		<td rowspan="3" class="rotate-box"><div class="rotate-270">Всего</div></td>';
+				
+			html += '		<td colspan="3">Из них</td>';
+				
+			html += '		<td colspan="6">I курс</td>';
+			html += '		<td colspan="6">II курс</td>';
+			html += '		<td colspan="6">III курс</td>';
+			html += '		<td colspan="6">IV курс</td>';
+			html += '		<td colspan="6">V курс</td>';
+
+			html += '	</tr>';
+			
+			html += '	<tr>';
+			html += '		<td rowspan="2" class="rotate-box medium"><div class="rotate-270">Лекции</div></td>';
+			html += '		<td rowspan="2" class="rotate-box medium"><div class="rotate-270">Лабораторные</div></td>';
+			html += '		<td rowspan="2" class="rotate-box medium"><div class="rotate-270">Семинары</div></td>';
+				
+			html += '		<td rowspan="2" colspan="3"><div>I семестр</div><div>17 недель</div></td>';
+			html += '		<td rowspan="2" colspan="3"><div>II семестр</div><div>17 недель</div></td>';
+			html += '		<td rowspan="2" colspan="3"><div>III семестр</div><div>17 недель</div></td>';
+			html += '		<td rowspan="2" colspan="3"><div>IV семестр</div><div>17 недель</div></td>';
+			html += '		<td rowspan="2" colspan="3"><div>V семестр</div><div>17 недель</div></td>';
+			html += '		<td rowspan="2" colspan="3"><div>VI семестр</div><div>17 недель</div></td>';
+			html += '		<td rowspan="2" colspan="3"><div>VII семестр</div><div>16 недель</div></td>';
+			html += '		<td rowspan="2" colspan="3"><div>VIII семестр</div><div>16 недель</div></td>';
+			html += '		<td rowspan="2" colspan="3"><div>IX семестр</div><div>16 недель</div></td>';
+			html += '		<td rowspan="2" colspan="3"><div>X семестр</div><div>7 недель</div></td>';
+			html += '	</tr>';
+
+			html += '	<tr>';
+			html += '	</tr>';
+
+			html += '	<tr>';
+			html += '		<td><b>1</b></td>';
+			html += '		<td><b>2</b></td>';
+			html += '		<td><b>3</b></td>';
+			html += '		<td><b>4</b></td>';
+			html += '		<td><b>5</b></td>';
+			html += '		<td><b>6</b></td>';
+			html += '		<td><b>7</b></td>';
+			html += '		<td><b>8</b></td>';
+			html += '		<td><b>9</b></td>';
+			html += '		<td><b>10</b></td>';
+			html += '		<td><b>11</b></td>';
+			html += '		<td><b>12</b></td>';
+			html += '		<td><b>13</b></td>';
+			html += '		<td><b>14</b></td>';
+			html += '		<td><b>15</b></td>';
+			html += '		<td><b>16</b></td>';
+			html += '		<td><b>17</b></td>';
+			html += '		<td><b>18</b></td>';
+			html += '		<td><b>19</b></td>';
+			html += '		<td><b>20</b></td>';
+			html += '		<td><b>21</b></td>';
+			html += '		<td><b>22</b></td>';
+			html += '		<td><b>23</b></td>';
+			html += '		<td><b>24</b></td>';
+			html += '		<td><b>25</b></td>';
+			html += '		<td><b>26</b></td>';
+			html += '		<td><b>27</b></td>';
+			html += '		<td><b>28</b></td>';
+			html += '		<td><b>29</b></td>';
+			html += '		<td><b>30</b></td>';
+			html += '		<td><b>31</b></td>';
+			html += '		<td><b>32</b></td>';
+			html += '		<td><b>33</b></td>';
+			html += '		<td><b>34</b></td>';
+			html += '		<td><b>35</b></td>';
+			html += '		<td><b>36</b></td>';
+			html += '		<td><b>37</b></td>';
+			html += '		<td><b>38</b></td>';
+			html += '		<td><b>39</b></td>';
+			html += '		<td><b>40</b></td>';
+			html += '		<td><b>41</b></td>';
+			html += '		<td><b>42</b></td>';
+			html += '	</tr>';
+
+			for (var i = 0; i < education.filler.database.length; i++)
+			{
+				html += '<tr>';
+
+				html += '	<td>' + rude.romanize(i + 1) + '</td>';
+				html += '	<td>' + education.filler.database[i] + '</td>';
+
+				html += '	<td class="input department"><div class="ui form"><div class="inline field"><input class="20" type="text" maxlength="3" value=""></div></div></td>';
+
+				for (var j = 0; j < 39; j++)
+				{
+					html += '	<td class="input countable"><div class="ui form"><div class="inline field"><input onblur="education.filler.recount(this)" onkeyup="education.filler.recount(this)" class="20" type="text" maxlength="3" value=""></div></div></td>';
+				}
+
+				html += '</tr>';
+			}
+
+
+			html += '<tr class="empty no-border"><td class="no-border" colspan="42"></td></tr>';
+
+
+			html += '<tr class="result">';
+
+			html += '<td colspan="3">Итого:</td>';
+
+			for (i = 0; i < 39; i++)
+			{
+				html += '<td>0</td>';
+			}
+
+			html += '</tr>';
+
+
+			html += '</tbody>';
+			html += '</table>';
+
+			html += '<div class="ui divider"></div><div class="ui green submit button small" onclick="education.filler.save();">Сохранить</div>';
+
+
+			$('#filler-modal .content').html(html);
+
+			$('.rotate-270').rotate(270);
+		},
+
+		save: function()
+		{
+			var disciplines = education.filler.database;
+			var name = education.filler.name;
+
+			var values = education.filler.recount();
+		},
+
+		recount: function(selector)
+		{
+			var result = [];
+
+			var selector_table = $(selector).closest('table');
+
+			selector_table.find('td.countable input').each(function() {
+					result.push($(this).val());
+			});
+
+			debug(result);
+
+			for (var i = 0; i < 39; i++)
+			{
+				var sum = 0;
+
+				for (var j = 0; j < (result.length / 39); j++)
+				{
+					var val = parseInt(result[i + (j * 39)]);
+
+					if (isNaN(val))
+					{
+						continue;
+					}
+
+					sum += val;
+				}
+
+
+				var selector_td = selector_table.find('tr.result td:eq(' + (i + 1) + ')');
+
+				selector_td.html(sum);
+
+				if (sum == 0)
+				{
+					selector_td.addClass('empty').removeClass('non-empty');
+				}
+				else
+				{
+					selector_td.addClass('non-empty').removeClass('empty');
+				}
+			}
+
+			return result;
+		},
+
 		get: function(selector)
 		{
 			var result = [];
@@ -50,7 +257,7 @@ var education =
 		subclass += '<li class="disciplines">';
 
 		subclass += '<div class="actions">';
-		subclass += '	<div class="ui button red tiny" onclick="$(this).closest(\'li\').fadeToggle(\'slow\', function() { $(this).closest(\'li\').remove(); buttons.update(); });">Удалить</div><div class="ui button blue tiny" onclick="education.filler.popup(education.filler.get(this));">Заполнить</div>';
+		subclass += '	<div class="ui button red tiny" onclick="$(this).closest(\'li\').fadeToggle(\'slow\', function() { $(this).closest(\'li\').remove(); buttons.update(); });">Удалить</div><div class="ui button blue tiny" onclick="education.filler.popup(education.filler.get(this), $(this).closest(\'.disciplines\').find(\'.description\').html());">Заполнить</div>';
 		subclass += '</div>';
 
 		subclass += '	<div class="base" onclick="$(this).parent(\'li\').find(\'.tip\').toggle(\'slow\'); $(this).find(\'i.icon.triangle\').toggleClass(\'down\').toggleClass(\'right\')">';
@@ -93,8 +300,6 @@ var education =
 
 		$(education.selector_ul).append(subclass);
 
-
-//		education.update();
 
 		rude.semantic.init.dropdown();
 	},
